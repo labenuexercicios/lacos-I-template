69a679617633de0889d62963ac0ee493b1f0b115
guirozmesquita@gmail.com <guilhermesquita>
2022-09-12 09:27:13 -0400

    exe03

--- script.js
+++ script.js
@@ -12,9 +12,17 @@ console.log('////////////////////////')
 
 ///////////////////////////
 
+//EXE02
 alert('Vamos fazer agora uma contagem')
 let numberUser = Number(prompt('Digite seu número para contagem'))
 
-for (i = 1; i < numberUser + 1; i++){
+for (i = 1; i < numberUser + 1; i++) {
     console.log(i)
+}
+
+//EXE03
+const arrNumbers = [3, 5, 4, 2, 19, 10, 1]
+
+for (i = 0; i < arrNumbers.length; i++) {
+    console.log(arrNumbers[i])
 }
--- teste.js
+++ teste.js
@@ -1,5 +1,5 @@
-let numberUser = 5
+const arrNumbers = [3, 5, 4, 2, 19, 10, 1]
 
-for (i = 0; i < numberUser + 1; i++){
-    console.log(i)
+for(i = 0; i < arrNumbers.length; i++){
+    console.log(arrNumbers[i])
 }
