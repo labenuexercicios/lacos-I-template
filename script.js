91feb9f4c978d00811d82dcf44879ef23b5a67a8
guirozmesquita@gmail.com <guilhermesquita>
2022-09-12 09:08:49 -0400

    exe02

--- script.js
+++ script.js
@@ -2,12 +2,19 @@
 let userName;
 let sum = 0;
 
-while(userName !== 0){
+while (userName !== 0) {
     userName = Number(prompt('Insira um N°'));
     sum += userName;
 }
 
 console.log(sum)
+console.log('////////////////////////')
 
+///////////////////////////
 
-//
+alert('Vamos fazer agora uma contagem')
+let numberUser = Number(prompt('Digite seu número para contagem'))
+
+for (i = 1; i < numberUser + 1; i++){
+    console.log(i)
+}
---
+++ teste.js
@@ -0,0 +1,5 @@
+let numberUser = 5
+
+for (i = 0; i < numberUser + 1; i++){
+    console.log(i)
+}
