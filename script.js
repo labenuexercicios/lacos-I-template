30cc76cc54f228cc311ff7ce669e063bbd46047b
euleo11 <leonardov.silva15@gmail.com>
2022-09-15 19:55:39 -0300

    atividade da aula15

---
+++ script.js
@@ -0,0 +1,19 @@
+// let userInput
+// let soma = 0
+
+// while(userInput !== 0) {
+//     userInput = Number(prompt("Insira um numero qualquer"))
+//     soma = soma + userInput
+//     console.log(soma)
+// }
+
+// const userInput = Number(prompt("Insira um numero qualquer"))
+// for(let i = 0; i <= userInput; i++) {
+//     console.log(i)
+// }
+
+let listaDeNumeros = [1, 2, 3, 4, 5, 6, 7]
+
+for(let i = 0; i <listaDeNumeros.length; i++){
+    console.log(`O número do índice ${i} é ${listaDeNumeros[i]}`)
+}
