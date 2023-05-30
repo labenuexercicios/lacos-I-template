e09a5a802289d4b5333f96405c874d839f3a6077
Daylton Araujo <dayltongg46@gmail.com>
2023-05-29 21:09:25 -0300

    lacos I

---
+++ script.js
@@ -0,0 +1,49 @@
+// let numero = Number(prompt("Digite um número"))
+
+// // //while (condicao){
+// //         block de codigo
+// //         incremento
+// // }
+
+// // let soma = 0
+
+// // while(numero !== 0 ){ //!== diferente de zero
+// //     soma = soma +numero
+// //     console.log(numero)
+// //     console.log(soma)
+// //     numero = Number(prompt("digite um novo numero"))
+// // }
+
+// // console.log("Soma:", soma)
+// //FOR
+
+// // for(let i = 0; i <= numero; i++){
+// //     console.log(i)
+// // }
+
+// const array = [4,89, 23, 10 , 6 ,65 , 2]
+
+// for(let i=0; i < array.length; i++) {
+//     console.log(`O elemento de índice ${i} é ${array[i]}`)
+// }
+//1.
+// let usuario = (prompt("Digite o seu tipo de usuario")).toUpperCase()
+
+// while(usuario !== "A"){
+//     console.log("Acesso negado!")
+//     tipoUsuario = prompt("Informe seu usuário: ")
+// }
+// console.log("Boas vindas, admin!")
+
+//2.
+// let tabuada = 2
+
+// for (let i = 0; i <= 10; i++ ){
+//     console.log(i * 2);
+// }
+//
+
+const slaaoq = ["oi", "oie", "oiee", "oieeee", "nah", "okay?", "letra maisucula!!", "verdade"]
+for(let i = 0 ; i < slaaoq.length ; i++){
+    console.log(slaaoq[i].toUpperCase())
+}
