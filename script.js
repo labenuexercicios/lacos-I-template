c3a0362b72b240ac5b7aaf3c5d661ca47cc0d67a
Douglasluis508 <douglasluis508@gmail.com>
2023-06-04 13:15:10 -0300

    Aula de lacos parte I

---
+++ script.js
@@ -0,0 +1,43 @@
+//let numero =Number(prompt('Digite um número'))
+// let soma = 0
+// while(numero !== 0){
+//     soma += numero
+//     console.log(numero)
+//     numero =Number(prompt('Digite outro número'))
+// }
+// console.log(soma);
+
+////////////////////
+
+// let numero =Number(prompt('Digite um número'))
+// for(i = 0; i<=numero; i++){
+//     console.log(i)
+// }
+
+/////////////////////
+
+// let arrayTeste = [1,2,3,4,5,6,7]
+// for(i = 0; i<arrayTeste.length;i++){
+//     console.log(arrayTeste[i])
+// }
+
+/////////////
+
+    // let usuario = prompt('Qual seu usuário?')
+
+    // while(usuario !== 'A'){
+    //     console.log('Acesso negado')
+    //     usuario = prompt('Qual seu usario')
+    // }
+    // console.log('Boas vindas, admin!');
+
+////////////////
+
+    // for(i = 1; i <=10;i++){
+    //     console.log(`2x${i} = ${2*i}`)
+    // }
+
+    let arrayString = ['celular', 'caneta', 'Douglas', 'mouse', 'Pipoca']
+    for(i = 0; i <=arrayString.length;i++){
+        console.log(arrayString[i].toUpperCase());
+    }
